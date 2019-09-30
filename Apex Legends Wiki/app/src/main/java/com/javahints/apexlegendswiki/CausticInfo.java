a0f8ddb409f73a9d73b9a59434569d02aa5d05d9
Ivan Kosenkov <bouncer77rus@gmail.com>
2019-09-30 18:16:25 +0300

    Добавленна вторая активность CausticInfo

---
+++ Apex Legends Wiki/app/src/main/java/com/javahints/apexlegendswiki/CausticInfo.java
@@ -0,0 +1,14 @@
+package com.javahints.apexlegendswiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CausticInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caustic_info);
+    }
+}
